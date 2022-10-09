15d413fc97933e313c66de8a0c4246ae50de1ef5
Yuuta Segawa <yuuta@192.168.100.4>
2022-10-09 17:32:27 +0300

    Updates

--- src/js/components/dropdown.js
+++
@@ -1,9 +0,0 @@
-$('.dropdown').click(function () {
-  $(this).attr('tabindex', 1).focus();
-  $(this).toggleClass('active');
-  $(this).find('.dropdown-menu').slideToggle(250).css('display', 'flex');
-});
-$('.dropdown').focusout(function () {
-  $(this).removeClass('active');
-  $(this).find('.dropdown-menu').slideUp(250);
-});
